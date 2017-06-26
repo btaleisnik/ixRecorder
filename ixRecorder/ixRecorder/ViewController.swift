1274ee3a23eba8cbc0829ad093f2501e1d2406f3
Brandon Taleisnik <btaleisnik@gmail.com>
2017-06-26 16:14:04 +0200

    Created New Project

---
+++ ixRecorder/ixRecorder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ixRecorder
+//
+//  Created by Brandon Taleisnik on 6/26/17.
+//  Copyright © 2017 Brandon Taleisnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
