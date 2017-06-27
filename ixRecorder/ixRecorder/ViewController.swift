02abbc607fb5ac37a76cd915bc9d7e183226f64b
Brandon Taleisnik <btaleisnik@gmail.com>
2017-06-27 16:10:26 +0200

    Added microphone and stop button

--- ixRecorder/ixRecorder/ViewController.swift
+++ ixRecorder/ixRecorder/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startRecording(sender: AnyObject) {
+        print("Hello world")
+    }
 
 }
 
