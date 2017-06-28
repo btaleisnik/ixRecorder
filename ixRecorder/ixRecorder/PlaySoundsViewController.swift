3ca934ef50e38fd0267f72b5721dfb1042d63429
Brandon Taleisnik <btaleisnik@gmail.com>
2017-06-28 18:13:16 +0200

    fast and slow audio playback added

--- ixRecorder/ixRecorder/PlaySoundsViewController.swift
+++ ixRecorder/ixRecorder/PlaySoundsViewController.swift
@@ -7,20 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
     var recordedAudioURL: URL!
+    var testVar: String!
+    var audioPlayer: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print(testVar)
         // Do any additional setup after loading the view.
+        
+        do {
+            let sound = try AVAudioPlayer(contentsOf: recordedAudioURL)
+            audioPlayer = sound
+            sound.play()
+        } catch {
+            print("Couldn't load or play recording")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func fastPlayback(_ sender: Any) {
+        
+        do {
+            let sound = try AVAudioPlayer(contentsOf: recordedAudioURL)
+            audioPlayer = sound
+            
+            sound.enableRate = true
+            sound.rate = Float(1.5)
+            sound.play()
+        } catch {
+            print("Couldn't load or play recording")
+        }
+        
+        
+        
+    }
+    
+    
+    @IBAction func slowPlayback(_ sender: Any) {
+        
+        do {
+            let sound = try AVAudioPlayer(contentsOf: recordedAudioURL)
+            audioPlayer = sound
+            
+            sound.enableRate = true
+            sound.rate = Float(0.7)
+            sound.play()
+        } catch {
+            print("Couldn't load or play recording")
+        }
+        
     }
     
 
